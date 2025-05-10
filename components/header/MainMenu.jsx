06ec9a19eb389a6c0d8c907bfa857e3e75bfbb98
abhijeetmssn <[email protected]>
2025-05-10 16:22:43 +0530

    logo issue

--- components/header/MainMenu.jsx
+++ components/header/MainMenu.jsx
@@ -35,8 +35,8 @@ const MainMenu = () => {
       {/* End mobile collapse menu */}
 
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="d-block d-lg-none">
+        <ul className="navbar-nav mt-3">
+        <li className="d-none">
             <div className="logo">
               <Link href="/" className="d-block">
                 <Image
@@ -48,6 +48,7 @@ const MainMenu = () => {
               </Link>
             </div>
           </li>
+
           {/* End li */}
 
           <li className="nav-item dropdown mega-dropdown-md">
@@ -73,128 +74,7 @@ const MainMenu = () => {
               About us
             </Link>
           </li>
-          {/* End li (home mega menu) */}
-
-          {/* <li className="nav-item  dropdown">
-            <a
-              // className="nav-link dropdown-toggle"
-              className={
-                pagesItems.some((menu) =>
-                  menu.subItems.some((elm) => isActive(elm.link))
-                ) ||
-                  isActive("/pages-menu/pricing") ||
-                  isActive("/pages-menu/testimonials")
-                  ? "nav-link dropdown-toggle active-menu"
-                  : "nav-link dropdown-toggle"
-              }
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              data-bs-auto-close="outside"
-              aria-expanded="false"
-            >
-              Pages
-            </a>
-            <ul className="dropdown-menu">
-              {pagesItems.map((item, index) => (
-                <li className="dropdown-submenu dropdown" key={index}>
-                  <a
-                    // className="dropdown-item dropdown-toggle"
-                    data-bs-toggle="dropdown"
-                    data-bs-auto-close="outside"
-                    aria-expanded="false"
-                    className={
-                      item.subItems.some((elm) => isActive(elm.link))
-                        ? "dropdown-item dropdown-toggle active-menu"
-                        : "dropdown-item dropdown-toggle"
-                    }
-                    href="#"
-                  >
-                    <span>{item.title}</span>
-                  </a>
-                  <ul className="dropdown-menu">
-                    {item.subItems.map((subMenu, i) => (
-                      <li key={i}>
-                        <Link
-                          href={subMenu.link}
-                          className={`dropdown-item ${isActive(subMenu.link) ? "active" : ""
-                            }`}
-                        >
-                          <span>{subMenu.title}</span>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          </li> */}
-          {/* End li (pages) */}
-
-          {/* <li className="nav-item dropdown">
-            <a
-              // className="nav-link dropdown-toggle"
-              className={
-                portfolioItems.some((elm) => isActive(elm.link))
-                  ? "nav-link dropdown-toggle active-menu"
-                  : "nav-link dropdown-toggle"
-              }
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              data-bs-auto-close="outside"
-              aria-expanded="false"
-            >
-              Portfolio
-            </a>
-            <ul className="dropdown-menu">
-              {portfolioItems.map((item, index) => (
-                <li key={index}>
-                  <Link
-                    href={item.link}
-                    className={`dropdown-item ${isActive(item.link) ? "active" : ""
-                      }`}
-                  >
-                    <span>{item.name}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li> */}
-          {/* End li (portfolio) */}
-
-          {/* <li className="nav-item dropdown">
-            <a
-              // className="nav-link dropdown-toggle"
-              className={
-                blogItems.some((elm) => isActive(elm.link))
-                  ? "nav-link dropdown-toggle active-menu"
-                  : "nav-link dropdown-toggle"
-              }
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              data-bs-auto-close="outside"
-              aria-expanded="false"
-            >
-              Blog
-            </a>
-            <ul className="dropdown-menu">
-              {blogItems.map((blog, index) => (
-                <li key={index}>
-                  <Link
-                    href={blog.link}
-                    className={`dropdown-item ${isActive(blog.link) ? "active" : ""
-                      }`}
-                  >
-                    <span>{blog.text}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li> */}
-          {/* End li (blog) */}
-
+          
           <li className="nav-item">
             <a
               className={
@@ -212,16 +92,7 @@ const MainMenu = () => {
         {/* End ul */}
 
         {/* Mobile Content */}
-        <div className="mobile-content d-block d-lg-none">
-          <div className="d-flex flex-column align-items-center justify-content-center mt-70">
-            <Link
-              href="/contact/contact-v1"
-              className="btn-twentyOne fw-500 tran3s"
-            >
-              Contact us
-            </Link>
-          </div>
-        </div>
+        
         {/* /.mobile-content */}
       </div>
     </nav>
