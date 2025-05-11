143713d29fabb7b997cfae244a646c5212e77c10
abhijeetmssn <[email protected]>
2025-05-11 14:14:02 +0530

    added email service

--- app/contact/contact-v3/page.jsx
+++ app/contact/contact-v3/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 
 import CopyrightFooter2 from "@/components/footer/CopyrightFooter2";
@@ -6,23 +7,41 @@ import NewsLetter from "@/components/footer/NewsLetter";
 import Header2 from "@/components/header/Header2";
 import BlockContact3 from "@/components/contact/BlockContact3";
 import ContactForm3 from "@/components/contact/ContactForm3";
-export const metadata = {
-  title: "Contact || CA",
-};
+import { ToastContainer, toast } from 'react-toastify';
+
 const ContactV3 = () => {
+  const handleShowToast = (message, error) => {
+    if (error) {
+      toast.error(message, {
+        closeButton: false
+      });
+    } else {
+      toast.success(message, {
+        closeButton: false
+      });
+    }
+  };
   return (
     <>
-      {/* <!-- 
-      =============================================
-      Theme Default Menu
-      ============================================== 	
-      --> */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000} // Adjust this as needed
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        style={{
+          zIndex: 2147483647,
+          position: "fixed",
+          top: "1rem",
+          right: "1rem"
+        }}
+      />
       <Header2 />
-      {/* 
-			=============================================
-				Contact Section Three
-			============================================== 
-			*/}
+      
       <div className="contact-section-three p-30">
         <div className="address-wrapper zn2 position-relative pt-180 lg-pt-150 mb-150 lg-mb-80">
           <div className="container">
@@ -72,7 +91,7 @@ const ContactV3 = () => {
 
             <div className="col-xxl-11 m-auto">
               <div className="form-style-four" data-aos="fade-up">
-                <ContactForm3 />
+                <ContactForm3 handleShowToast={handleShowToast}/>
               </div>
               {/* /.form-style-four */}
             </div>
--- components/contact/ContactForm3.jsx
+++ components/contact/ContactForm3.jsx
@@ -1,54 +1,102 @@
+"use client";
+import emailjs from '@emailjs/browser';
+import { useRef, useState} from "react";
 import React from "react";
 
-const ContactForm3 = () => {
+const ContactForm3 = ({ handleShowToast }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+
+  const formRef = useRef(null);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formRef.current)
+    emailjs.sendForm('service_6wa09kk', 'template_ujlv4bk', formRef.current, 'oKbgzOJ2Qlwws3zA5')
+      .then((result) => {
+        handleShowToast("Message Sent!", false);
+        console.log("yo")
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+          phone: ""
+        });
+      }, (error) => {
+        handleShowToast("Problem Occured!", true);
+      });
+      //formRef.reset();
+  };
+
   return (
-    <form action="#">
+    <form onSubmit={handleSubmit} ref={formRef}>
       <div className="messages" />
+
       <div className="input-bg-group position-relative controls">
+        {/* Name */}
         <div className="d-block">
           <div className="input-group-meta form-group mb-50">
-            <label className="d-block">Name*</label>
+            <label htmlFor="name" className="d-block">Name*</label>
             <input
-              type="text"
-              placeholder="Your name"
-              name="name"
-              required="required"
-            />
+                type="text"
+                placeholder="Your Name*"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required="required"
+                data-error="Name is required."
+              />
             <div className="help-block with-errors" />
           </div>
         </div>
-        {/* End .d-block */}
 
+        {/* Email */}
         <div className="d-block">
           <div className="input-group-meta form-group mb-60">
-            <label className="d-block">Email*</label>
+            <label htmlFor="email" className="d-block">Email*</label>
             <input
-              type="email"
-              placeholder="Your email"
-              name="email"
-              required="required"
-            />
+                type="email"
+                placeholder="Email Address*"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required="required"
+                data-error="Valid email is required."
+              />
             <div className="help-block with-errors" />
           </div>
         </div>
-        {/* End .d-block */}
 
+        {/* Message */}
         <div className="d-block">
           <div className="input-group-meta form-group">
+            <label htmlFor="message" className="d-block">Message*</label>
             <textarea
-              placeholder="Your message*"
-              name="message"
-              required="required"
-              defaultValue={""}
-            />
+                placeholder="Your message*"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required="required"
+                data-error="Please,leave us a message."
+              />
             <div className="help-block with-errors" />
           </div>
         </div>
-        {/* End .d-block */}
       </div>
-      {/* /.input-bg-group */}
 
-      <button className="btn-one fw-500 w-100 fs-18 d-block mt-45">
+      {/* Submit Button */}
+      <button type="submit" className="btn-one fw-500 w-100 fs-18 d-block mt-45">
         Send Message
       </button>
     </form>
